7ce09d33ccecb90f02086b1909502bc845179501
Benjamin <benjamin.trolais@octopuslab.fr>
2024-03-11 10:24:23 +0100

    add: login screen in stack

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import FakeForm from "../components/FakeForm";
+import LoginScreen from "../screens/LoginScreen";
 
 const Stack = createStackNavigator();
 
@@ -8,6 +9,7 @@ const StackNavigator = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="FakeForm" component={FakeForm} />
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
     </Stack.Navigator>
   );
 };
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -7,9 +7,9 @@ import { useNavigation } from "@react-navigation/native";
 // TODO: Create the screen with Formik form and Yup validation
 const LoginScreen = () => {
   return (
-    <View
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    ></View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>BONJOUR</Text>
+    </View>
   );
 };
 
