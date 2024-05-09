8a753cbdc029d672952cfb353ec2e5f0e119a133
Théophile Crespo Arredondo <theophile.crespo-arredondo@t2iservices.com>
2024-05-09 21:56:26 +0200

    get token and refresh token

--- src/screens/UserProfileScreen.js
+++ src/screens/UserProfileScreen.js
@@ -11,15 +11,16 @@ import {
 } from "react-native";
 import theme from "../config/theme";
 import { useNavigation } from "@react-navigation/native";
-import * as WebBrowser from 'expo-web-browser';
-const querystring = require('querystring');
-import 'url-search-params-polyfill';
-import * as Linking from 'expo-linking';
-import base64 from 'react-native-base64';
-var client_id = '7601ccc32cc449a39a85819a81b1cc4c';
-var client_secret = '7b3498d6ab3a4da0983d3ce5994e9cc7';
-var token = 'BQBpkxYG4R_2eLWav6p0rq4MG_94QQ5ehi5Q7YcVLI7x_Vm8NEHZ16d8VCHLnZjWTrGwmT5zw8ZBxsFwYJvbKHwEkGDPPPQUQmI3Koy5rESJXSRnoUnAkLvqJmnQchoiqfDNLcWH0NTz-oII296iEMgAJVFw1B-1JwksNRDz8fYYWCljO6bEswQQpEidSG1QhsNw-mBlQltipZIaYWQ53EG5Tg-PczqPpVuFXuUJyQUIe2b7DYhndnNlKEY6TIxx35z7ZZjERcg4oleuZ0Kd4j2fduL1R0zxAshbqA';
-var refresh_token = 'A'
+import * as WebBrowser from "expo-web-browser";
+const querystring = require("querystring");
+import "url-search-params-polyfill";
+import * as Linking from "expo-linking";
+import base64 from "react-native-base64";
+var client_id = "7601ccc32cc449a39a85819a81b1cc4c";
+var client_secret = "7b3498d6ab3a4da0983d3ce5994e9cc7";
+var token =
+  "BQBpkxYG4R_2eLWav6p0rq4MG_94QQ5ehi5Q7YcVLI7x_Vm8NEHZ16d8VCHLnZjWTrGwmT5zw8ZBxsFwYJvbKHwEkGDPPPQUQmI3Koy5rESJXSRnoUnAkLvqJmnQchoiqfDNLcWH0NTz-oII296iEMgAJVFw1B-1JwksNRDz8fYYWCljO6bEswQQpEidSG1QhsNw-mBlQltipZIaYWQ53EG5Tg-PczqPpVuFXuUJyQUIe2b7DYhndnNlKEY6TIxx35z7ZZjERcg4oleuZ0Kd4j2fduL1R0zxAshbqA";
+var refresh_token = "A";
 import { db, auth } from "../config/firebaseConfig";
 import { collection, doc } from "firebase/firestore";
 import { fetchUserById } from "../config/dbCalls";
@@ -57,64 +58,67 @@ export default function UserProfileScreen(route) {
   const isCurrentUserProfile = currentUser.uid === route.route.params.user_id;
 
   const [isConnected, setIsConnected] = useState(false);
-  const buttonText = isConnected ? 'Connected to Spotify!' : 'ajout de mon compte spotify';
-  var scope = 'user-read-playback-state user-read-currently-playing playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-top-read user-library-read user-library-modify user-read-recently-played';
-  
+  const buttonText = isConnected
+    ? "Connected to Spotify!"
+    : "ajout de mon compte spotify";
+  var scope =
+    "user-read-playback-state user-read-currently-playing playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-top-read user-library-read user-library-modify user-read-recently-played";
 
   const authorizeWithSpotify = async () => {
-
-    var redirect_uri = Linking.createURL('/--/spotify-callback'); //'exp://pembne0.anonymous.8081.exp.direct/--/--/spotify-callback';//http://localhost:19000/callback;
+    var redirect_uri = Linking.createURL("/--/spotify-callback"); //'exp://pembne0.anonymous.8081.exp.direct/--/--/spotify-callback';//http://localhost:19000/callback;
     console.log("redirect_uri:", redirect_uri);
     try {
       var state = generateRandomString(16);
-      const loginUrl = 'https://accounts.spotify.com/authorize?' +
-      querystring.stringify({
-        response_type: 'code',
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri,
-        state: state
-      });
-  
-      const listener  = Linking.addEventListener("url", ({ url }) => {
+      const loginUrl =
+        "https://accounts.spotify.com/authorize?" +
+        querystring.stringify({
+          response_type: "code",
+          client_id: client_id,
+          scope: scope,
+          redirect_uri: redirect_uri,
+          state: state,
+        });
+
+      const listener = Linking.addEventListener("url", ({ url }) => {
         console.log("URLb:", url);
         if (url.startsWith(redirect_uri)) {
           const urlParams = new URLSearchParams(url.split("?")[1]);
           const code = urlParams.get("code");
           const state = urlParams.get("state");
-  
+
           console.log("Authorization code:", code);
           console.log("State:", state);
 
           var authOptions = {
-            url: 'https://accounts.spotify.com/api/token',
+            url: "https://accounts.spotify.com/api/token",
             form: {
               code: code,
               redirect_uri: redirect_uri,
-              grant_type: 'authorization_code'
+              grant_type: "authorization_code",
             },
             headers: {
-              'content-type': 'application/x-www-form-urlencoded',
-              'Authorization': 'Basic ' + base64.encode(client_id + ':' + client_secret),
+              "content-type": "application/x-www-form-urlencoded",
+              Authorization:
+                "Basic " + base64.encode(client_id + ":" + client_secret),
             },
           };
 
           console.log("fetch Post");
           fetch(authOptions.url, {
-            method: 'POST',
+            method: "POST",
             headers: authOptions.headers,
-            body: querystring.stringify(authOptions.form)
+            body: querystring.stringify(authOptions.form),
           })
-            .then(response => response.json())
-            .then(data => {
-              console.log("Response data:", data);  // Log entire response data
+            .then((response) => response.json())
+            .then((data) => {
+              console.log("Response data:", data); // Log entire response data
               console.log("Access token:", data.access_token);
               console.log("Refresh token:", data.refresh_token);
               refresh_token = data.refresh_token;
               setIsConnected(true);
             })
-            .catch(error => {
-              console.error('Error:', error);
+            .catch((error) => {
+              console.error("Error:", error);
             });
 
           listener.remove; // Remove listener after handling the URL
@@ -123,7 +127,7 @@ export default function UserProfileScreen(route) {
       console.log("loginUrl:", loginUrl);
       const ret = await WebBrowser.openAuthSessionAsync(loginUrl);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
   if (isCurrentUserProfile) {
@@ -136,24 +140,33 @@ export default function UserProfileScreen(route) {
             <Text style={styles.infosText}>{email}</Text>
             <Text style={styles.infosTitle}>Token:</Text>
             <Text style={styles.infosText}>Token Spotify non valide</Text>
-            <TouchableOpacity style={styles.button}
-            onPress={get_refresh_token(refresh_token)}
-            >
-              <Text style={styles.buttonText}>refresh Spotify token</Text>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Se connecter à Spotify</Text>
             </TouchableOpacity>
           </View>
 
-        <ScrollView style={styles.favoriteContainer}>
-          <Text style={styles.favoriteTitle}>ma connexion:</Text>
-          <View style={styles.buttonContainer}>
-            <TouchableOpacity
-              style={styles.button}
-              onPress={authorizeWithSpotify}
-            >
-              <Text style={styles.buttonText}>{buttonText}</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
+          <ScrollView style={styles.favoriteContainer}>
+            <Text style={styles.favoriteTitle}>ma connexion:</Text>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={authorizeWithSpotify}
+              >
+                <Text style={styles.buttonText}>{buttonText}</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+          <ScrollView style={styles.favoriteContainer}>
+            <Text style={styles.favoriteTitle}>refresh token:</Text>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => get_refresh_token(refresh_token)}
+              >
+                <Text style={styles.buttonText}>{"nouveau token"}</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
           <ScrollView style={styles.favoriteContainer}>
             <Text style={styles.favoriteTitle}>Mes favoris:</Text>
             <Text style={styles.favoriteText}>Aucun favori pour le moment</Text>
@@ -220,8 +233,9 @@ export default function UserProfileScreen(route) {
 
 // Fonction pour générer une chaîne aléatoire de longueur donnée
 function generateRandomString(length) {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
