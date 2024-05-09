fa1a5df187ef19ca53fadcc5777afddffd2b5058
unknown <delacrepaul@gmail.com>
2024-05-09 15:41:14 +0200

    💥 Upvote work (need to manage user upvoted posts)

--- src/components/Upvote.js
+++ src/components/Upvote.js
@@ -4,78 +4,101 @@ import LottieView from "lottie-react-native";
 import { auth, db } from "../config/firebaseConfig";
 import {
   getDoc,
-  getDocs,
   setDoc,
   doc,
-  collection,
-  query,
-  where,
-  arrayContains,
+  arrayUnion,
+  arrayRemove,
+  increment,
 } from "@firebase/firestore";
 import theme from "../config/theme";
 
 export default function UpvoteButton({ postId }) {
   const animRef = useRef(null);
-  const [upvoted, setUpvoted] = useState(false);
   const currentUser = auth.currentUser;
+  const [likedPosts, setLikedPosts] = useState([]);
   const [upvotes, setUpvotes] = useState(0);
+  const [upvoted, setUpvoted] = useState(false);
 
   useEffect(() => {
-    const fetchUserLikedPosts = async () => {
+    const fetchUserLikedPostsAndUpvotes = async () => {
       try {
         const userDocRef = doc(db, "users", currentUser.uid);
         const userDoc = await getDoc(userDocRef);
         const userData = userDoc.data();
-        const likedPostsRefs = userData.likedPosts;
-        const likedPosts = [];
+        setLikedPosts(userData.likedPosts || []);
 
-        for (const postRef of likedPostsRefs) {
-          const postDoc = await getDoc(postRef);
-          likedPosts.push(postDoc.data());
-        }
-        const likedPostIds = likedPosts.map((post) => post.pid);
-        if (likedPostIds) {
-          setUpvoted(likedPostIds.includes(postId));
+        const postDocRef = doc(db, "posts", postId);
+        const postDoc = await getDoc(postDocRef);
+        const postData = postDoc.data();
+        setUpvotes(postData.upvotes || 0);
+
+        if (Array.isArray(userData.likedPosts)) {
+          const likedPostIds = userData.likedPosts.map((post) => post.pid);
+          if (likedPostIds) {
+            setUpvoted(likedPostIds.includes(postId));
+          }
         }
       } catch (error) {
-        console.error("Error fetching user liked posts:", error);
+        console.error("Error fetching user liked posts and upvotes:", error);
       }
     };
 
     if (currentUser) {
-      fetchUserLikedPosts();
-      if (upvoted) {
-        animRef.current.play();
-      }
+      fetchUserLikedPostsAndUpvotes();
     }
   }, [currentUser, postId]);
 
-  useEffect(() => {
-    const fetchPostUpvotes = async () => {
-      try {
-        const postDocRef = doc(db, "posts", postId);
-        const postDoc = await getDoc(postDocRef);
-        const postData = postDoc.data();
-        const upvotes = postData.upvotes;
-        setUpvotes(upvotes);
-      } catch (error) {
-        console.error("Error fetching post likes:", error);
-      }
-    };
-
-    fetchPostUpvotes();
-  }, [postId]);
-
-  const handlePress = () => {
+  const handlePress = async () => {
     setUpvoted(!upvoted);
-    if (upvoted) {
+    const postDocRef = doc(db, "posts", postId);
+    const userDocRef = doc(db, "users", currentUser.uid);
+
+    if (!upvoted) {
       animRef.current.play();
-      // setUpvotes(upvotes + 1);
-      // //TODO: Add back here
+      // Ajouter le postId à la liste des posts aimés par l'utilisateur
+      await setDoc(
+        userDocRef,
+        {
+          likedPosts: arrayUnion(postId),
+        },
+        { merge: true }
+      );
+
+      // Augmenter le nombre d'upvotes du post
+      await setDoc(
+        postDocRef,
+        {
+          upvotes: increment(1),
+        },
+        { merge: true }
+      );
+      // Update local state
+      setUpvotes(upvotes + 1);
+      setLikedPosts([...likedPosts, { pid: postId }]);
+      setUpvoted(true);
     } else {
       animRef.current.reset();
-      // setUpvotes(upvotes - 1);
-      // //TODO: Add back here
+      // Supprimer le postId de la liste des posts aimés par l'utilisateur
+      await setDoc(
+        userDocRef,
+        {
+          likedPosts: arrayRemove(postId),
+        },
+        { merge: true }
+      );
+
+      // Diminuer le nombre d'upvotes du post
+      await setDoc(
+        postDocRef,
+        {
+          upvotes: increment(-1),
+        },
+        { merge: true }
+      );
+      // Update local state
+      setUpvotes(upvotes - 1);
+      setLikedPosts(likedPosts.filter((post) => post.pid !== postId));
+      setUpvoted(false);
     }
   };
 
@@ -114,7 +137,6 @@ const styles = StyleSheet.create({
     height: 50,
     borderColor: theme.colors.primary,
     borderWidth: 1,
-    
   },
   upvoteText: {
     marginLeft: -40,
